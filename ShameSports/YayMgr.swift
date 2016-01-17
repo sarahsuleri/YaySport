ec1d027fa46cc1286774ab05df2ec92de12dfbc1
dariaRWTH <khorkunova@gmail.com>
2016-01-17 21:26:00 +0100

    YayMgr Lost Functions

--- ShameSports/YayMgr.swift
+++ ShameSports/YayMgr.swift
@@ -70,8 +70,86 @@ class YayMgr {
     
     static func loadPost(snapshot: FDataSnapshot, tag: String) {
         
-
+        // Get Poster ID
+        let posterID = snapshot.value["Poster"] as! Int
         
+        // Get Points
+        let points = snapshot.value["Points"] as! [Int]
+        
+        // Get Post Owner
+        getUserByID(posterID) { poster in
+            
+            // Get Message
+            let textDict = snapshot.value["Text"] as! NSDictionary
+            let title = textDict.valueForKey("Title") as! String
+            let messageID = textDict.valueForKey("Message") as! Int
+            getMessageByID(messageID, title: title) { text in
+                
+                // Get Comments
+                loadComments(Int(snapshot.key)!) { comments in
+                    
+                    // Create a Post
+                    let post = Post(Poster: poster, Points: points, Comments: comments, Text: text)
+                    
+                    if tag == "friends" {
+                        FrPosts.insert(post, atIndex: 0)
+                    } else {
+                        myPosts.insert(post, atIndex: 0)
+                    }
+                    print("FrPosts size: ", FrPosts.count)
+                }
+            }
+        }
+    }
+    
+    
+    static func getUserByID(id: Int, completionHandler: (User) -> ()) {
+        let posterRef = firebase.childByAppendingPath("users/\(id)")
+        posterRef.observeEventType(FEventType.Value) { (snapshot: FDataSnapshot!) -> Void in
+            let poster = User(Id: id,
+                FirstName: snapshot.value["FirstName"] as! String,
+                LastName: snapshot.value["LastName"] as! String,
+                PhotoUrl: snapshot.value["PhotoUrl"] as! String
+            )
+            completionHandler(poster)
+        }
+    }
+    
+    
+    static func getMessageByID(id: Int, title: String, completionHandler: (Message) -> ()) {
+        let messageRef = firebase.childByAppendingPath("messages/\(id)")
+        messageRef.observeEventType(FEventType.Value) { (snapshot: FDataSnapshot!) -> Void in
+            let message = Message(Id: id,
+                Title: title,
+                Description: snapshot.value["Description"] as! String,
+                Yay: snapshot.value["Yay"] as! Bool
+            )
+            completionHandler(message)
+        }
+    }
+    
+    
+    static func loadComments(postId: Int, completionHandler: ([Comment]) -> ()) {
+        var comments = [Comment]()
+        let commentsRef = firebase.childByAppendingPath("comments")
+        
+        commentsRef.queryOrderedByChild("PostID").queryEqualToValue(postId).observeEventType(.Value, withBlock: { snapshot in
+            
+            let loadCommentorGroup = dispatch_group_create()
+            let enumerator = snapshot.children
+            while let snapChild = enumerator.nextObject() as? FDataSnapshot {
+                dispatch_group_enter(loadCommentorGroup)
+                getUserByID(snapChild.value!["Commentor"] as! Int) { commentor in
+                    let comment = Comment(Commentor: commentor,
+                        Comment: snapChild.value!["Comment"] as! String)
+                    comments.append(comment)
+                    dispatch_group_leave(loadCommentorGroup)
+                }
+            }
+            dispatch_group_notify(loadCommentorGroup, dispatch_get_main_queue()) {
+                completionHandler(comments)
+            }
+        })
     }
     
     static func getBooMsg() -> String{
